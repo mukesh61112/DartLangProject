eec4a5d18a34f44ef3acc3ba24667529eb833689
mukesh111 <MUKESH61112@GMAIL.COM>
2022-06-16 11:22:53 +0530

    dart

---
+++ .idea/libraries/Dart_1_startTheory.dart
@@ -0,0 +1,88 @@
+void main(){
+
+
+
+
+
+   int a=5;
+   print("${a}");
+   print('${a}');
+
+   int hexvalue=0xEADEBAEE;
+   print(hexvalue);
+
+
+   double ad=645;
+   print(ad);
+
+   double exponents=1.5e5;
+   print(exponents);
+
+
+   double af=978.789;
+   print(af);
+
+
+    var c=645;
+    print(c);
+
+    var d=564.654;
+    print(d);
+
+
+    var e=true;
+    print(e);
+
+    var f="kjkla";
+    print(f);
+
+    var g='a';
+    print(g);
+
+    // var h=j;  give error
+    // print(h);
+
+
+    String  mass="kasdjfkl";
+    print(mass);
+
+
+
+
+   print("kajsfkljkasdjfkjs");
+   print('kajskldfjkdsj');
+   print(5);
+   print(86633);
+   print(6545.65456);
+   print(false);
+   print(true);
+
+
+   int b;
+   b=897;
+   print(b);
+
+   b=a;
+   print(b);
+
+   print(5+5);
+   print(5-5);
+   print(5*54);  //kjkljkljkljklj
+   print(5/78);
+   print(8/5);
+   print(100%10);
+
+
+   //lkjkljkljjlkj
+   /* kjkjkjk
+   jjkljkljkjklj
+
+   lkjkkjkj
+   lkkjkj
+    */
+
+    int z=654;
+    int y=4798;
+    print("${z+y}");
+    print(" sum of $z and $y is ${z+y}");
+}
